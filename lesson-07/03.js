35c11b088a220b14a05bdc50cb7bfda74bebcdba
Ivan Zhevniak <146636885+ivanycla@users.noreply.github.com>
2025-01-23 21:47:52 +0300

    Update 03.js

--- lesson-07/03.js
+++ lesson-07/03.js
@@ -1,9 +1,8 @@
-function truncate(str, maxLength) {  
-   
-    if (str.length > maxLength) {  
-        
-        return str.slice(0, maxLength - 3) + '...'; 
-    }  
-    
-    return str;   
-}  
+function truncate(str, maxLength) {
+  if (str.length > maxLength) {
+    return str.slice(0, maxLength) + '...';
+  } else {
+    return str;
+  }
+}
+
